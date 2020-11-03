aa120ee191e5b4f337340a123a4a10303e0fe7a5
Alex Sherman <asherman.ca@gmail.com>
2020-11-03 09:59:02 -0800

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Landing Page</h1>
+    </>
+  )
+}
+
+export default App
---
+++ notes.md
@@ -0,0 +1,4 @@
+Terminal Commands:
+removing hidden files
+ls -a to find hidden files
+`rm -rf {filename}` to remove files
