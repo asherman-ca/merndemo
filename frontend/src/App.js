68f2c134354e48a8284420879c61db6c9262c0b9
Alex Sherman <asherman.ca@gmail.com>
2020-11-05 19:22:51 -0800

    header, footer, product components and home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <>
-      <h1>Landing Page</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
--- notes.md
+++ notes.md
@@ -2,3 +2,12 @@ Terminal Commands:
 removing hidden files
 ls -a to find hidden files
 `rm -rf {filename}` to remove files
+
+React Snippet:
+rafce to make an exported arrow component
+
+React-Bootstrap:
+the classname py-7 means padding on the y axis (top/bottom) of 7
+
+New Resource Site:
+https://cdnjs.com/
