4d015ac562d1dd0d6e917a7eaae28679ceb96cf5
Alex Sherman <asherman.ca@gmail.com>
2020-11-09 15:45:27 -0800

    backend product fetching

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -10,18 +16,22 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+// app.get('/api/products', (req, res) => {
+//   res.json(products)
+// })
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+// app.get('/api/products/:id', (req, res) => {
+//   const product = products.find(p => p._id === req.params.id)
+//   res.json(product)
+// })
+
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server Environment: ${process.env.NODE_ENV}\nPort: ${PORT}`)
+  console.log(
+    `Server Environment: ${process.env.NODE_ENV}\nPort: ${PORT}`.yellow.bold
+  )
 )
