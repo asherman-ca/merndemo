0ea5eb3eec68ce3819fb1fec1df55d3cb5748110
Alex Sherman <asherman.ca@gmail.com>
2020-11-13 15:38:44 -0800

    login route working

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+// import products from './data/products.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -13,21 +14,16 @@ connectDB()
 
 const app = express()
 
+// json body parser middleware
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-// app.get('/api/products', (req, res) => {
-//   res.json(products)
-// })
-
-// app.get('/api/products/:id', (req, res) => {
-//   const product = products.find(p => p._id === req.params.id)
-//   res.json(product)
-// })
-
 // Middlewares (routes are kinda middleware)
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
