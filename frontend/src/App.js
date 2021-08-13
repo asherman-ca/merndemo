6c7c38516932ff04cf3a5ce3d16b8591931b4add
Alex Sherman <asherman.ca@gmail.com>
2021-08-13 00:39:40 -0700

    admin product list FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/productList' component={ProductListScreen} />
 
           <Route exact path='/cart' component={CartScreen} />
           <Route path='/cart/:id' component={CartScreen} />
