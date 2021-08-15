051f10e4a6d1be5192d0c2d6538393998d5222cd
Alex Sherman <asherman.ca@gmail.com>
2021-08-15 01:45:29 -0700

    create review BE

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import colors from 'colors'
 import connectDB from './config/db.js'
 // import products from './data/products.js'
@@ -19,6 +20,10 @@ const app = express()
 
 // json body parser middleware
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.get('/', (req, res) => {
   res.send('API is running...')
