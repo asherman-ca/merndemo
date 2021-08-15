45ec79b05afd50ddffbe1c2f9dae546ef7119b7a
Alex Sherman <asherman.ca@gmail.com>
2021-08-14 19:33:23 -0700

    product edit screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
+
 
 const App = () => {
   return (
@@ -32,6 +34,7 @@ const App = () => {
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/productList' component={ProductListScreen} />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
 
           <Route exact path='/cart' component={CartScreen} />
           <Route path='/cart/:id' component={CartScreen} />
